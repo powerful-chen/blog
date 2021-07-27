8cffabd36bd41cde518c0643ce9b9dd246044f27
powerful-chen <2930763626@qq.com>
2021-07-28 07:28:57 +0800

    添加管理后台

---
+++ blog-paren/blog-admin/src/main/java/com/chen/blog/admin/AdminApp.java
@@ -0,0 +1,17 @@
+package com.chen.blog.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName AdminApp
+ * @Description TODO
+ * @Author xiaochen
+ * @Date 2021/7/27 9:38
+ */
+@SpringBootApplication
+public class AdminApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApp.class, args);
+    }
+}
